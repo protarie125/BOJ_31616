5440d3be0426b610962807c399ac4ccda3192f34
protarie125 <protarie@gmail.com>
2024-06-02 22:37:40 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <bit>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <ranges>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = int64_t;
+using vl = vector<ll>;
+
+ll n;
+string s;
+
+int main() {
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  cin >> n >> s;
+  const auto& b = s.front();
+  for (const auto& x : s) {
+    if (x != b) {
+      cout << "No";
+      return 0;
+    }
+  }
+
+  cout << "Yes";
+
+  return 0;
+}
